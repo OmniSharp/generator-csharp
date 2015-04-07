6db479b04b661ad959f2b52aeb958459bd1760a7
Shmueli Englard <shmueli.yosef@englard.net>
2015-04-06 23:47:30 -0400

    Made versions of files for each framework version as needed

---
+++ app/templates/classlibrary/Class1.20.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace <%= namespace %>
+{
+    public class Class1
+    {
+    }
+}
---
+++ app/templates/classlibrary/Class1.35.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace <%= namespace %>
+{
+    public class Class1
+    {
+    }
+}
